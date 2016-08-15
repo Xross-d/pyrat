92805fde6db38f0efa381d4b8a6c1662d26a090e
Xross <Xrossd.1@gmail.com>
2016-08-15 09:07:26 +0200

    Update librat.py

--- librat.py
+++ librat.py
@@ -2,3 +2,16 @@
 #
 #librat.py implements all algorithms used in rat.py including encryption.
 #
+
+def search_for_files():
+	"""
+	search for files in the current dirctory.
+	"""
+	pass
+
+def collect_files():
+	"""
+	parse and save all files returned by search_for_files.
+	"""
+	fileList = search_for_files()
+	pass
