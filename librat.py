518db9ba6b8a3b70e010489b222e2a795139f6ee
Xross <Xrossd.1@gmail.com>
2016-08-15 09:08:37 +0200

    Update librat.py

--- librat.py
+++ librat.py
@@ -3,6 +3,17 @@
 #librat.py implements all algorithms used in rat.py including encryption.
 #
 
+def check_file_size(arg, dirname, files):
+	for file in files:
+		filename = os.path.join(dirname,file)
+		if os.path.isfile(filename):
+			size = os.path.getsize(filename)
+			if size > 10000000:
+				if arg is None:
+					print '%.2fMB %s'%(size/10000000.0,filename)
+				elif isinstance(arg,list):
+					arg.append((size/10000000.0,filename))
+
 def search_for_files():
 	"""
 	search for files in the current dirctory.
